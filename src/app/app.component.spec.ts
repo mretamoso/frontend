8aac6ee958a4c0a4b5ad86ed0e043e1ef123e6d4
Alonso <miguelretamosoin@gmail.com>
2023-01-30 22:30:18 -0500

    Angular14front

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('frontend');
   });
 
-  it('should render title', () => {
+  it('deberia renderizar un parrafo', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('hola hola');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    setTimeout(() => {
+
+      this.peliculasEnCines = [{
+        titulo: 'Spider-Man',
+        fechaLanzamiento: new Date(),
+        precio: 1400.99
+      },
+      {
+        titulo: 'Michi-Man',
+        fechaLanzamiento: new Date(),
+        precio: 2000.00
+      },
+      {
+        titulo: 'Iron-Man',
+        fechaLanzamiento: new Date('2016-11-14'),
+        precio: 865.00
+      }]
+    }, 1000);
+  }
+  title = 'frontend como quiero eh';
+
+  peliculasEnCines;
+
+  peliculasProximosEstrenos = [{
+    titulo: 'Coco',
+    fechaLanzamiento: new Date(),
+    precio: 1400.99
+  },
+  {
+    titulo: 'Luna',
+    fechaLanzamiento: new Date(),
+    precio: 2000.00
+  },
+  {
+    titulo: 'Varyan',
+    fechaLanzamiento: new Date('2016-11-14'),
+    precio: 865.00
+  },
+  {
+    titulo: 'KiroBeagles',
+    fechaLanzamiento: new Date('2016-11-14'),
+    precio: 865.00
+  }]
+
+
+  duplicarNumero(valor: number): number {
+    return valor * 2;
+  }
 }
